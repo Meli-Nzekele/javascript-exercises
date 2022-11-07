14018823e45033e7e9da34087f81b4a657406345
Meli-Nzekele <melissanzekelecourtney@gmail.com>
2022-11-07 16:23:38 +0000

    Exercise complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (word, numOfTimes) {
+  if (numOfTimes < 0) return "ERRORnpm test repeatString.spec.js";
+  let str = "";
+  for (let i = 0; i < numOfTimes; i++) {
+    str += word;
+  }
+  return str;
 };
 
 // Do not edit below this line
